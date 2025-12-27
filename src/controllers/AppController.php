7d682f7e43853d74d474c4cc35d6a6d1d105e27d
Jakub Jachowicz <jakub.jachowicz@medius.com>
2025-12-27 19:55:50 +0100

    pijwojanka

--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -1,22 +1,31 @@
 <?php
 
-class AppController 
-{
+
+class AppController {
+
+    protected function isGet(): bool
+    {
+        return $_SERVER["REQUEST_METHOD"] === 'GET';
+    }
+
+    protected function isPost(): bool
+    {
+        return $_SERVER["REQUEST_METHOD"] === 'POST';
+    }
+    
     protected function render(string $template = null, array $variables = [])
     {
         $templatePath = 'public/views/'. $template.'.html';
         $templatePath404 = 'public/views/404.html';
         $output = "";
                  
-        if (file_exists($templatePath))
-        {
+        if(file_exists($templatePath)){
             extract($variables);
             
             ob_start();
             include $templatePath;
             $output = ob_get_clean();
-        } else 
-        {
+        } else {
             ob_start();
             include $templatePath404;
             $output = ob_get_clean();
--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -1,18 +1,136 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController
 {
+    private $userRepository;
+    
+    public function __construct()
+    {
+        $this->userRepository = new UserRepository();
+    }
+    
+    private static $instance = null;
+
+    private function __clone() {}
+    public function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize singleton");
+    }
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    // public function login()
+    // {
+    //     if ($this->isPost())
+    //     {
+    //         var_dump($_POST);
+            
+    //         $email = $_POST['email'] ?? '';
+    //         $password = $_POST['password'] ?? '';
+
+    //         return $this->render('dashboard');
+
+    //         // $url = "http://$_SERVER[HTTP_HOST]";
+    //         // header("Location: {$url}/dashboard");
+    //     }
+        
+    //     return $this->render('login', ['error' => 'Invalid credentials']);
+    // }
+
+    // public function register()
+    // {
+    //     return $this->render('register');
+    // }
+
+    private static array $users = [
+        [
+            'email' => 'anna@example.com',
+            'password' => '$2y$10$wz2g9JrHYcF8bLGBbDkEXuJQAnl4uO9RV6cWJKcf.6uAEkhFZpU0i', // test123
+            'first_name' => 'Anna'
+        ],
+        [
+            'email' => 'bartek@example.com',
+            'password' => '$2y$10$fK9rLobZK2C6rJq6B/9I6u6Udaez9CaRu7eC/0zT3pGq5piVDsElW', // haslo456
+            'first_name' => 'Bartek'
+        ],
+        [
+            'email' => 'celina@example.com',
+            'password' => '$2y$10$Cq1J6YMGzRKR6XzTb3fDF.6sC6CShm8kFgEv7jJdtyWkhC1GuazJa', // qwerty
+            'first_name' => 'Celina'
+        ],
+    ];
+
+
     public function login()
     {
-        // TODO Get data from login form and process it
-        
-        return $this->render('login');
+        if (!$this->isPost()) {
+            return $this->render('login');
+        }
+
+        $email = $_POST["email"] ?? '';
+        $password = $_POST["password"] ?? '';
+
+        if (empty($email) || empty($password)) {
+            return $this->render('login', ['messages' => 'Fill all fields']);
+        }
+
+        $userRow = $this->userRepository->getUserByEmail($email);
+
+        if (!$userRow) {
+            return $this->render('login', ['messages' => 'User not found']);
+        }
+
+        if (!password_verify($password, $userRow['password'])) {
+            return $this->render('login', ['messages' => 'Wrong password']);
+        }
+
+        // TODO możemy przechowywać sesje użytkowika lub token
+        // setcookie("username", $userRow['email'], time() + 3600, '/');
+
+        $url = "http://$_SERVER[HTTP_HOST]";
+        header("Location: {$url}/dashboard");
     }
 
     public function register()
     {
-        return $this->render('register');
+        if (!$this->isPost()) {
+            return $this->render('register');
+        }
+
+        $email = trim($_POST['email'] ?? '');
+        $password = $_POST['password'] ?? '';
+        $password2 = $_POST['password2'] ?? '';
+        $firstName = $_POST['firstname'] ?? '';
+
+        if (empty($email) || empty($password) || empty($password2) || empty($firstName)) {
+            return $this->render('register', ['messages' => 'Fill all fields']);
+        }
+
+        if ($password !== $password2) {
+            return $this->render('register', ['messages' => 'Passwords do not match']);
+        }
+
+	// TODO this will be checked in database
+        foreach (self::$users as $u) {
+            if (strcasecmp($u['email'], $email) === 0) {
+                return $this->render('register', ['messages' => 'Email is taken']);
+            }
+        }
+
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+        $this->userRepository->createUser($email, $hashedPassword, $firstName);
+
+        return $this->render('login', ['messages' => 'Registration successful. Please log in.']);
     }
 }
